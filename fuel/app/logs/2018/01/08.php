0c60be0e918899947b8b110a00ff3a2d66f7be05
lopez0398 <jorge_lopez_apps1m1617@cev.com>
2018-01-08 06:51:24 -0500

    commit inicial

---
+++ fuel/app/logs/2018/01/08.php
@@ -0,0 +1,3 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+WARNING - 2018-01-08 06:21:35 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
